d10cd159d856afba50b1b61ee720732bd2cbdb00
Marc <marc.purgimon@gmail.com>
2021-06-30 19:41:08 +0200

    Search Activity: created xml searchable and menu xml as well as method on Search Activity

--- app/src/main/java/com/example/flickrbrowser/SearchActivity.kt
+++ app/src/main/java/com/example/flickrbrowser/SearchActivity.kt
@@ -1,11 +1,17 @@
 package com.example.flickrbrowser
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
 
 
 class SearchActivity : BaseActivity() {
     private val TAG = "SearchActivity"
+    private var searchView: SearchView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         Log.d(TAG, "onCreate starts")
@@ -14,5 +20,22 @@ class SearchActivity : BaseActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
         //activateToolbar(true)
         Log.d(TAG, "onCreate ends")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d(TAG, "onCreateOptionsMenu: starts")
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val searchManager= getSystemService(Context.SEARCH_SERVICE) as SearchManager //Get the class to handle the Search Manager
+        searchView= menu.findItem(R.id.app_bar_search).actionView as SearchView //Get the reference to the appBar
+        val searchableInfo = searchManager.getSearchableInfo(componentName)
+        searchView?.setSearchableInfo(searchableInfo)
+
+        Log.d(TAG, ".onCreateOptionsMenu $componentName")
+        Log.d(TAG, ".onCreateOptionsMenu hint is ${searchView?.queryHint}")
+        Log.d(TAG, ".onCreateOptionsMenu $searchableInfo")
+
+        searchView?.isIconified = true
+        return true
     }
 }
